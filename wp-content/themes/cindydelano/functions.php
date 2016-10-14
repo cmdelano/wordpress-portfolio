9d66e1b3e45d0d78a2693c36256d17809dbec525
cmdelano <cmdelano@gmail.com>
2016-10-14 11:15:21 -0400

    added PT Sans font to site as non-heading font

--- wp-content/themes/cindydelano/functions.php
+++ wp-content/themes/cindydelano/functions.php
@@ -41,3 +41,15 @@ register_post_type( 'about',
 
 // Hooks the custom post type function up to the theme
 add_action( 'init', 'create_custom_post_types' );
+
+
+// PT Sans: https://fonts.google.com/specimen/PT+Sans
+
+function google_fonts() {
+    $query_args = array(
+        'family' => 'PT+Sans:400,400i,700,700i'
+        );
+    wp_register_style( 'google_fonts', add_query_arg( $query_args, "//fonts.googleapis.com/css" ), array(), null );
+            }
+            
+add_action('wp_enqueue_scripts', 'google_fonts');
